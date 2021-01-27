5372cd52656146bfa7a9a308b501e7977918ffe3
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-01-27 10:47:24 +0700

    Adding comment documentation and improve and update feature without material design component.

--- field/src/main/java/com/github/fajaragungpramana/field/FieldInput.kt
+++ field/src/main/java/com/github/fajaragungpramana/field/FieldInput.kt
@@ -2,16 +2,38 @@ package com.github.fajaragungpramana.field
 
 import android.content.Context
 import android.os.Build
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import androidx.core.graphics.drawable.DrawableCompat
 
+/**
+ * FieldInput
+ *
+ * FieldInput is layout component base on ViewGroup LinearLayout for get input from user.
+ *
+ * @property primaryColor = get application colorPrimary from theme.
+ * @property mView = for initialize layout.
+ * @property drawableEnd = for set icon in the right side of field input max width 24dp and height 24dp.
+ * @property errorMessage = to set error message at the bottom of field input.
+ * @property focus = set focus of field input. set true its mean user can type false user can't type.
+ * @property hint = set field hint.
+ * @property inputType = set input type of field.
+ * @property style = set text appearance of field.
+ * @property text = to get or set text of field.
+ * @property textAllCaps = to set caps of field. set true is cap, set false is not.
+ * @property textColor = to set text color of field.
+ * @property textSize = to set text size of field.
+ */
 class FieldInput @JvmOverloads constructor(
     private val ctx: Context,
     attrs: AttributeSet?,
@@ -19,17 +41,27 @@ class FieldInput @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : LinearLayout(ctx, attrs, defStyle, defStyleRes) {
 
-    private lateinit var mTextInputLayout: TextInputLayout
-    private lateinit var mTextInputEditText: TextInputEditText
+    private lateinit var mHint: TextView
+    private lateinit var mFieldInput: EditText
+    private lateinit var mError: TextView
     private lateinit var mDrawableEnd: ImageView
+
+    private val primaryColor: Int
+        get() {
+            val typedValue = TypedValue()
+            ctx.theme.resolveAttribute(R.attr.colorPrimary, typedValue, true)
+
+            return typedValue.data
+        }
 
     private var mView: View? = null
         set(value) {
             field = value
 
             field?.let {
-                mTextInputLayout = it.findViewById(R.id.til_field_input)
-                mTextInputEditText = it.findViewById(R.id.tie_field_input)
+                mHint = it.findViewById(R.id.tv_field_hint)
+                mFieldInput = it.findViewById(R.id.et_field_input)
+                mError = it.findViewById(R.id.tv_field_error)
                 mDrawableEnd = it.findViewById(R.id.iv_field_drawable_end)
             }
         }
@@ -45,25 +77,65 @@ class FieldInput @JvmOverloads constructor(
         set(value) {
             field = value
 
-            mTextInputLayout.errorIconDrawable = null
-            if (mTextInputLayout.isErrorEnabled) mTextInputLayout.error = field
+            mError.text = field
+
+            DrawableCompat.setTint(
+                mFieldInput.background,
+                if (field != null)
+                    setColor(android.R.color.holo_red_light)
+                else
+                    primaryColor
+            )
+            mHint.setTextColor(
+                if (field != null)
+                    setColor(android.R.color.holo_red_light)
+                else
+                    primaryColor
+            )
         }
 
     var focus: Boolean = true
         set(value) {
             field = value
 
-            mTextInputEditText.isFocusable = focus
+            mFieldInput.isFocusable = focus
         }
 
     var hint: String? = null
         set(value) {
             field = value
 
-            mTextInputEditText.hint = hint
-            mTextInputEditText.setOnFocusChangeListener { _, hasFocus ->
-                mTextInputEditText.hint = if (hasFocus) null else hint
-                mTextInputLayout.hint = if (hasFocus) hint else null
+            mHint.text = hint
+            mFieldInput.hint = hint
+
+            mFieldInput.setOnFocusChangeListener { _, hasFocus ->
+
+                mFieldInput.hint = if (hasFocus) null else hint
+
+                if (text.isNotEmpty())
+                    mHint.animate()
+                        .alpha(1F)
+                        .translationY(10F)
+                        .setDuration(300)
+                        .start()
+                else
+                    mHint.animate()
+                        .alpha(if (hasFocus) 1F else 0F)
+                        .translationY(if (hasFocus) 10F else 30F)
+                        .setDuration(300)
+                        .start()
+
+                if (errorMessage == null) {
+                    mHint.setTextColor(if (hasFocus) primaryColor else setColor(android.R.color.darker_gray))
+
+                    DrawableCompat.setTint(
+                        mFieldInput.background,
+                        if (hasFocus)
+                            primaryColor
+                        else
+                            setColor(android.R.color.darker_gray)
+                    )
+                }
             }
         }
 
@@ -71,15 +143,7 @@ class FieldInput @JvmOverloads constructor(
         set(value) {
             field = value
 
-            mTextInputEditText.inputType = field
-        }
-
-    @Suppress("DEPRECATION")
-    var passwordToggleEnabled: Boolean = false
-        set(value) {
-            field = value
-
-            mTextInputLayout.isPasswordVisibilityToggleEnabled = field
+            mFieldInput.inputType = field
         }
 
     @Suppress("DEPRECATION")
@@ -89,39 +153,40 @@ class FieldInput @JvmOverloads constructor(
 
             if (field != 0)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                    mTextInputEditText.setTextAppearance(field)
+                    mFieldInput.setTextAppearance(field)
                 else
-                    mTextInputEditText.setTextAppearance(ctx, field)
+                    mFieldInput.setTextAppearance(ctx, field)
         }
 
-    var text: String? = null
-        get() = mTextInputEditText.text.toString()
+    var text: String = ""
+        get() = mFieldInput.text.toString()
         set(value) {
             field = value
 
-            if (!field.isNullOrEmpty()) mTextInputLayout.hint = hint
-            mTextInputEditText.setText(field)
+            if (field.isEmpty()) mFieldInput.hint = hint
+
+            mFieldInput.setText(field)
         }
 
     var textAllCaps: Boolean = false
         set(value) {
             field = value
 
-            mTextInputEditText.isAllCaps = field
+            mFieldInput.isAllCaps = field
         }
 
     var textColor: Int = 0
         set(value) {
             field = value
 
-            if (field != 0) mTextInputEditText.setTextColor(ContextCompat.getColor(ctx, field))
+            if (field != 0) mFieldInput.setTextColor(setColor(field))
         }
 
     var textSize: Float = 0F
         set(value) {
             field = value
 
-            if (field != 0F) mTextInputEditText.textSize = field - 50F
+            if (field != 0F) mFieldInput.textSize = field / 2F
         }
 
     init {
@@ -133,7 +198,6 @@ class FieldInput @JvmOverloads constructor(
         focus = ta.getBoolean(R.styleable.FieldInput_focusable, true)
         hint = ta.getString(R.styleable.FieldInput_hint)
         inputType = ta.getInt(R.styleable.FieldInput_inputType, InputType.TYPE_CLASS_TEXT)
-        passwordToggleEnabled = ta.getBoolean(R.styleable.FieldInput_passwordToggleEnabled, false)
         style = ta.getResourceId(R.styleable.FieldInput_style, 0)
         textAllCaps = ta.getBoolean(R.styleable.FieldInput_textAllCaps, false)
         textColor = ta.getResourceId(R.styleable.FieldInput_textColor, 0)
@@ -142,6 +206,12 @@ class FieldInput @JvmOverloads constructor(
         ta.recycle()
     }
 
+    /**
+     * Do action when user click on drawable
+     *
+     * @param drawablePosition = set with drawable you want to action click. DrawablePosition(START or END)
+     * @param onClickDrawableListener = higher order function lambda, to do something when user clicked the drawable of field.
+     */
     fun setOnClickDrawableListener(
         drawablePosition: DrawablePosition,
         onClickDrawableListener: () -> Unit
@@ -154,6 +224,33 @@ class FieldInput @JvmOverloads constructor(
         }
     }
 
+    /**
+     * Get the text or do something when user is still typing.
+     *
+     * @param text = higher order function lambda, to get real time text input
+     */
+    fun setOnTextChanged(text: (String) -> Unit) {
+        mFieldInput.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                text(s.toString())
+            }
+
+        })
+    }
+
+    /**
+     * Set color for component needed.
+     */
+    private fun setColor(color: Int) = ContextCompat.getColor(ctx, color)
+
+    /**
+     * Destroy component when detached from window
+     */
     override fun onDetachedFromWindow() {
         mView = null
         super.onDetachedFromWindow()
