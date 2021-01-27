2112f26e66c7fcfad051de5ef6bad840e927a4a7
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-01-27 10:44:43 +0700

    Adding comment documentation.

--- field/src/main/java/com/github/fajaragungpramana/field/DrawablePosition.kt
+++ field/src/main/java/com/github/fajaragungpramana/field/DrawablePosition.kt
@@ -1,5 +1,11 @@
 package com.github.fajaragungpramana.field
 
+/**
+ * This enum to check and handle drawable position in event listener
+ *
+ * @property START = its mean, you want action listener in drawable left position of field.
+ * @property END = its mean, you wan action listener in drawable right position of field.
+ */
 enum class DrawablePosition {
     START,
     END
